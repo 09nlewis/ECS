e663ade89db29d882404af2288ef7b84862d8a1f
09nlewis <simranlewis@hotmail.co.uk>
2020-02-13 15:43:24 +0000

    Set up of pages

---
+++ Schedule.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Event Schedule</title>
+<link rel="stylesheet" type="text/css" href=Website5.css >
+
+</head>
+<body>
+<header> 
+<div class="container">
+<h1>Event Schedule </h1>
