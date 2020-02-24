0f8f85ee448aba4a0477a02d56aa7b8bb1909872
09nlewis <simranlewis@hotmail.co.uk>
2020-02-24 17:07:05 +0000

    changed to .php

--- Event_Creation.php
+++ Event_Creation.php
@@ -1,12 +1,358 @@
 <!DOCTYPE html>
 <html>
-<head>
+<title>Event creation</title>
 <meta charset="UTF-8">
-<title>Event Creation</title>
-<link rel="stylesheet" type="text/css" href=Website5.css >
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
+<style>
+body, html {
+  height: 100%;
+  font-family: "Inconsolata", sans-serif;
+}
 
-</head>
+.bgimg {
+  background-position: center; 
+  background-size: cover;
+  background-image: url("https://github.com/09nlewis/ECS/blob/master/Banner.jpeg?raw=true");
+  min-height: 75%;
+}
+
+.menu {
+  display: none;
+}
+</style>
 <body>
-<header> 
-<div class="container">
-<h1>Event Creation </h1>
+
+<!-- Links (sit on top) -->
+<div class="w3-top">
+  <div class="w3-row w3-padding w3-black">
+    <div class="w3-col s3">
+      <a href="index.html" class="w3-button w3-block w3-black">HOME</a>
+    </div>
+    <div class="w3-col s3">
+      <a href="Event_Creation.html" class="w3-button w3-block w3-black">EVENT CREATION</a>
+    </div>
+    <div class="w3-col s3">
+      <a href="Competition_Entry.html" class="w3-button w3-block w3-black">COMPETITION ENTRY</a>
+    </div>
+    <div class="w3-col s3">
+      <a href="Shedule.html" class="w3-button w3-block w3-black">SCHEDULE</a>
+    </div>
+  </div>
+</div>
+
+<!-- Header with image -->
+<header class="bgimg w3-display-container w3-grayscale-min" id="home">
+  <div class="w3-display-bottomleft w3-center w3-padding-large w3-hide-small">
+    <span class="w3-tag">Natalie Lewis</span>
+  </div>
+  <div class="w3-display-middle w3-center">
+  </div>
+  <div class="w3-display-bottomright w3-center w3-padding-large">
+  </div>
+</header>
+
+<!-- Add a background color and large text to the whole page -->
+<div class="w3-sand w3-grayscale w3-large">
+
+<html>
+  <head>
+    <title>Event Creation</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <style>
+      html, body {
+      min-height: 100%;
+      }
+      body, div, form, input, select, textarea, label, p { 
+      padding: 0;
+      margin: 0;
+      outline: none;
+      font-family: Roboto, Arial, sans-serif;
+      font-size: 14px;
+      color: #666;
+      line-height: 22px;
+      }
+      h1 {
+      position: absolute;
+      margin: 0;
+      font-size: 34px;
+      color: #fff;
+      z-index: 2;
+      line-height: 83px;
+      }
+      textarea {
+      width: calc(100% - 12px);
+      padding: 5px;
+      }
+      .testbox {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: inherit;
+      padding: 20px;
+      }
+      form {
+      width: 100%;
+      padding: 20px;
+      border-radius: 6px;
+      background: #fff;
+      box-shadow: 0 0 8px  #669999; 
+      }
+      .banner {
+      position: relative;
+      height: 300px;
+      background-image: url("/uploads/media/default/0001/02/50d7a918b0cd14570b3281e08c5ddfb7d299332e.jpeg");  
+      background-size: cover;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+      }
+      .banner::after {
+      content: "";
+      background-color: rgba(0, 0, 0, 0.2); 
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      }
+      input, select, textarea {
+      margin-bottom: 10px;
+      border: 1px solid #ccc;
+      border-radius: 3px;
+      }
+      input {
+      width: calc(100% - 10px);
+      padding: 5px;
+      }
+      input[type="date"] {
+      padding: 4px 5px;
+      }
+      textarea {
+      width: calc(100% - 12px);
+      padding: 5px;
+      }
+      .item:hover p, .item:hover i, .question:hover p, .question label:hover, input:hover::placeholder {
+      color:  #669999;
+      }
+      .item input:hover, .item select:hover, .item textarea:hover {
+      border: 1px solid transparent;
+      box-shadow: 0 0 3px 0  #669999;
+      color: #669999;
+      }
+      .item {
+      position: relative;
+      margin: 10px 0;
+      }
+      .item span {
+      color: red;
+      }
+      input[type="date"]::-webkit-inner-spin-button {
+      display: none;
+      }
+      .item i, input[type="date"]::-webkit-calendar-picker-indicator {
+      position: absolute;
+      font-size: 20px;
+      color:  #a3c2c2;
+      }
+      .item i {
+      right: 1%;
+      top: 30px;
+      z-index: 1;
+      }
+      [type="date"]::-webkit-calendar-picker-indicator {
+      right: 1%;
+      z-index: 2;
+      opacity: 0;
+      cursor: pointer;
+      }
+      input[type=radio], input[type=checkbox]  {
+      display: none;
+      }
+      label.radio {
+      position: relative;
+      display: inline-block;
+      margin: 5px 20px 15px 0;
+      cursor: pointer;
+      }
+      .question span {
+      margin-left: 30px;
+      }
+      .question-answer label {
+      display: block;
+      }
+      label.radio:before {
+      content: "";
+      position: absolute;
+      left: 0;
+      width: 17px;
+      height: 17px;
+      border-radius: 50%;
+      border: 2px solid #ccc;
+      }
+      input[type=radio]:checked + label:before, label.radio:hover:before {
+      border: 2px solid  #669999;
+      }
+      label.radio:after {
+      content: "";
+      position: absolute;
+      top: 6px;
+      left: 5px;
+      width: 8px;
+      height: 4px;
+      border: 3px solid  #669999;
+      border-top: none;
+      border-right: none;
+      transform: rotate(-45deg);
+      opacity: 0;
+      }
+      input[type=radio]:checked + label:after {
+      opacity: 1;
+      }
+      .flax {
+      display:flex;
+      justify-content:space-around;
+      }
+      .btn-block {
+      margin-top: 10px;
+      text-align: center;
+      }
+      button {
+      width: 150px;
+      padding: 10px;
+      border: none;
+      border-radius: 5px; 
+      background:  #669999;
+      font-size: 16px;
+      color: #fff;
+      cursor: pointer;
+      }
+      button:hover {
+      background:  #a3c2c2;
+      }
+      @media (min-width: 568px) {
+      .name-item, .city-item {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      }
+      .name-item input, .name-item div {
+      width: calc(50% - 20px);
+      }
+      .name-item div input {
+      width:97%;}
+      .name-item div label {
+      display:block;
+      padding-bottom:5px;
+      }
+      }
+    </style>
+  </head>
+  <body>
+    <div class="testbox">
+      <form action="insert.php" method="post">
+        <div class="item" >
+            <label for="name">Event ID<span>*</span></label>
+            <input id="name" type="text" name="name" required/>
+          </div>
+        <div class="item">
+          <label for="name">Showground Name<span>*</span></label>
+          <input id="name" type="text" name="name" required/>
+        </div>
+        <div class="item">
+          <label for="email">Showground Address<span>*</span></label>
+          <input id="email" type="email" name="email" required/>
+        </div>
+        <div class="item">
+          <label for="attendees">Allow team entries<span>*</span></label>
+          <input id="attendess" type="text" name="attendees" required/>
+        </div>
+        <div class="item">
+          <label for="bdate">Set Date of Event<span>*</span></label>
+          <input id="bdate" type="date" name="bdate" required/>
+          <i class="fas fa-calendar-alt"></i>
+        </div>
+        <div class="flax">
+          <div class="item">
+            <p>Time of first class</p>
+            <select>
+              <option selected value="" disabled selected></option>
+              <option value="8A" >8 AM</option>
+              <option value="9A">9 AM</option>
+              <option value="10A">10 AM</option>
+              <option value="11A">11 Am</option>
+              <option value="12P">12 Pm</option>
+              <option value="1P">1 Pm</option>
+              <option value="2P">2 Pm</option>
+              <option value="3P">3 Pm</option>
+              <option value="4P">4 Pm</option>
+              <option value="5P">5 Pm</option>
+              <option value="6P">6 Pm</option>
+              <option value="7P">7 Pm</option>
+              <option value="8P">8 Pm</option>
+            </select>
+          </div>
+          <div class="item">
+            <p>Select classes the showground would like to run</p>
+            <select>
+              <option selected value="" disabled selected></option>
+              <option value="8A" >1ft6</option>
+              <option value="9A">1ft9</option>
+              <option value="10A">2ft</option>
+              <option value="11A">2ft3</option>
+              <option value="12P">2ft6</option>
+              <option value="1P">2ft9</option>
+              <option value="2P">3ft</option>
+              <option value="3P">3ft3</option>
+              <option value="4P">3ft6</option>
+              <option value="5P">3ft9</option>
+            </select>
+          </div>
+        </div>
+        <div class="item">
+          <label for="instructions">Special Instructions</label>
+          <textarea id="instructions" rows="3"></textarea>
+        </div>
+        <p><small>0/100 characters</small></p>
+        <div class="btn-block">
+          <button type="submit" href="/">Submit</button>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
+
+<?php
+echo 
+'<html>
+
+</html>';
+
+$con = mysqli_connect("csmysql.cs.cf.ac.uk", "c1609359", "watoocnob2", "c1609359");
+
+if(!$con){
+	die("failed to connect:" . mysqli_connect_error());
+}
+
+$query = "SELECT * FROM website"; 
+$result = mysqli_query($con,$query);
+while($row = mysqli_fetch_assoc($result)) {
+echo	'<div align="center" class="T-shirts">';
+echo		'<div class="T-shirt1">';
+echo			'<div><h1>'. $row['Name'] .'</h1></div>';
+echo				'<img class="image" src="'.$row['URL'].'" />';
+echo					'<p>£' . $row['Price'] . '</p>';
+echo						'<div>'. $row['id'] .'</div>';
+echo						'<div class="Link">'; 
+echo						'<div><h2><a target="_blank"> More Information</a></h2></div>';
+echo								'<div><h1><input type="text" name="quantity" value="1" size="2" /><input type="submit" value="Add to cart" class="btnAddAction" /><h1></div>';
+		
+echo		'</div>';
+echo	'</div>';
+echo '</body>';
+echo '</html>';
+}
+
+mysqli_close($con);
+?>
